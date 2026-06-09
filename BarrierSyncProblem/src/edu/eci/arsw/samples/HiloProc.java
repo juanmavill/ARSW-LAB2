24cdec13aaefdc9b04de45f86d9339f903d9b312
Juan Manuel Villegas Medina <juan.vmedina@mail.escuelaing.edu.co>
2026-06-08 23:01:27 -0500

    Add files via upload

---
+++ BarrierSyncProblem/src/edu/eci/arsw/samples/HiloProc.java
@@ -0,0 +1,48 @@
+package edu.eci.arsw.samples;
+
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+
+public class HiloProc extends Thread{
+
+	int waitPeriod=0;
+	int idHilo=0;
+	long resultado=0;
+	private final CountDownLatch barreraFinalizacion;
+	
+	public HiloProc(int id, CountDownLatch barreraFinalizacion){
+		this.barreraFinalizacion=barreraFinalizacion;
+		try {
+			Thread.sleep(10);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		waitPeriod=Math.abs(new Random(System.currentTimeMillis()).nextInt()%5000);
+		idHilo=id;
+	}
+	
+	public void run(){
+		int numit=10;
+		long startTime=System.currentTimeMillis();
+		try {
+			for (int i=0;i<numit;i++){
+				System.out.println("Soy el hilo "+idHilo+" y voy en el "+((float)((float)(i+1)/(float)numit)*100)+"% de mi tarea. P:"+waitPeriod);
+				try {
+					Thread.sleep(waitPeriod);
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			resultado=System.currentTimeMillis()-startTime;
+		} finally {
+			barreraFinalizacion.countDown();
+		}
+	}
+	
+	
+
+	public long getResultado() {
+		return resultado;
+	}
+}
---
+++ BarrierSyncProblem/src/edu/eci/arsw/samples/Main.java
@@ -0,0 +1,36 @@
+package edu.eci.arsw.samples;
+
+import java.util.concurrent.CountDownLatch;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int numHilos=20;
+		CountDownLatch barreraFinalizacion=new CountDownLatch(numHilos);
+		
+		HiloProc[] hilos=new HiloProc[numHilos];
+		
+		for (int i=0;i<numHilos;i++){
+			hilos[i]=new HiloProc(i, barreraFinalizacion);
+		}
+		for (int i=0;i<numHilos;i++){
+			hilos[i].start();
+		}
+
+		try {
+			barreraFinalizacion.await();
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			throw new RuntimeException(e);
+		}
+
+		long tiempoPromedio=0;
+		
+		for (int i=0;i<numHilos;i++){
+			tiempoPromedio+=hilos[i].getResultado();
+		}
+
+		System.out.println("El tiempo promedio de la ejecucion fue de:"+tiempoPromedio/numHilos);
+	}
+	
+}
